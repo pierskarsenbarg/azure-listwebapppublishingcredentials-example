b8a04f5e15ba3c3d0e4c5b5130eadf6f65bec039
Piers Karsenbarg <piers@pulumi.com>
2022-05-13 14:12:27 +0100

    First commit

---
+++ MyStack.cs
@@ -0,0 +1,48 @@
+using System.Threading.Tasks;
+using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using Pulumi.AzureNative.Web;
+using Pulumi.AzureNative.Web.Inputs;
+
+class MyStack : Stack
+{
+    public MyStack()
+    {
+        // Create an Azure Resource Group
+        var resourceGroup = new ResourceGroup("resourceGroup");
+
+        var appService = new AppServicePlan("functionApp-appService", new AppServicePlanArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Sku = new SkuDescriptionArgs
+            {
+                Name = "Y1",
+                Tier = "Dynamic",
+            },
+        });
+
+        var app = new WebApp("mywebapp", new WebAppArgs
+        {
+            ResourceGroupName = resourceGroup.Name,
+            Kind = "functionapp",
+            ServerFarmId = appService.Id
+        });
+
+        var credentials = ListWebAppPublishingCredentials.Invoke(new ListWebAppPublishingCredentialsInvokeArgs
+        {
+            Name = app.Name,
+            ResourceGroupName = resourceGroup.Name
+        });
+
+        this.PublishingUserName = credentials.Apply(x => x.PublishingUserName);
+        this.PublishingPassword = credentials.Apply(x => x.PublishingPassword);
+    }
+
+    [Output]
+    public Output<string> PublishingUserName { get; set; }
+
+    [Output]
+    public Output<string?> PublishingPassword { get; set; }
+}
